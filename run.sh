497c9a9d9ca8ff1093ff8bbf7e06068e3ca8590a
Damian Tykałowski <d47zm3@gmail.com>
2018-12-27 14:23:02 +0100

    disable comments
    Signed-off-by: Damian Tykałowski <d47zm3@gmail.com>

--- run.sh
+++ run.sh
@@ -16,13 +16,13 @@ prepare_workspace
 decho "[domain enumeration]"
 
 decho_green "[*] running amass..."
-#docker run -it --rm -v $( pwd )/workspace/${domain}/amass/:/output/ security-tools:amass --passive -d ${domain} -o /output/result.txt > $( pwd )/workspace/${domain}/amass/run.log
+docker run -it --rm -v $( pwd )/workspace/${domain}/amass/:/output/ security-tools:amass --passive -d ${domain} -o /output/result.txt > $( pwd )/workspace/${domain}/amass/run.log
 
 decho_green "[*] running sublist3r..."
-#docker run -it --rm -v $( pwd )/workspace/${domain}/sublist3r/:/output/ security-tools:sublist3r python /opt/sublist3r/sublist3r.py -d ${domain} -o /output/result.txt  > $( pwd )/workspace/${domain}/sublist3r/run.log
+docker run -it --rm -v $( pwd )/workspace/${domain}/sublist3r/:/output/ security-tools:sublist3r python /opt/sublist3r/sublist3r.py -d ${domain} -o /output/result.txt  > $( pwd )/workspace/${domain}/sublist3r/run.log
 
 decho_green "[*] running gobuster (brute force)... may take a while (10 minutes!)"
-#docker run -it --rm -v $( pwd)/dictionaries:/dic -v $( pwd )/workspace/${domain}/gobuster/:/output/ security-tools:gobuster gobuster -m dns -u ${domain} -t 50 -fw -w /dic/subdomains.txt -o /output/result.txt.tmp > $( pwd )/workspace/${domain}/gobuster/run.log
+docker run -it --rm -v $( pwd)/dictionaries:/dic -v $( pwd )/workspace/${domain}/gobuster/:/output/ security-tools:gobuster gobuster -m dns -u ${domain} -t 50 -fw -w /dic/subdomains.txt -o /output/result.txt.tmp > $( pwd )/workspace/${domain}/gobuster/run.log
 
 decho "[processing results...]"
 process_subdomains ${domain}
